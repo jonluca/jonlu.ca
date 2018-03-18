821a4081c88c58f64a30488246d0ed77ca6c6a93
JonLuca DeCaro <jdecaro@usc.edu>
2018-03-18 15:52:42 -0700

    reformatted

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,22 @@
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-
 module.exports = {
-    entry: './js/app.js',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.css$/,
-                loaders: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /(fonts|images)/,
-                loaders: [
-                    'url-loader'
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new UglifyJsPlugin({
-            test: /\.js($|\?)/i
-        })
-    ]
+  entry: './js/app.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.css$/,
+      loaders: ['style-loader', 'css-loader']
+    }, {
+      test: /(fonts|images)/,
+      loaders: ['url-loader']
+    }]
+  },
+  plugins: [new UglifyJsPlugin({
+    test: /\.js($|\?)/i
+  })]
 
 };
