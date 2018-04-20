d5ad93189543cea4dd696e4ca351f320fd892095
JonLuca DeCaro <jdecaro@usc.edu>
2018-04-19 18:41:48 -0700

    remake website, link to blog

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,28 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const ZopfliPlugin = require("zopfli-webpack-plugin");
 
 module.exports = {
-  entry: './js/app.js',
-  mode: 'production',
-  output: {
-    path: __dirname + '/dist',
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      test: /\.css$/,
-      loaders: ['style-loader', 'css-loader']
-    }, {
-      test: /(fonts|images)/,
-      loaders: ['url-loader']
-    }]
-  },
-  plugins: [new UglifyJsPlugin({
-    test: /\.js($|\?)/i
-  }), new ZopfliPlugin({
-    asset: "[path].gz[query]",
-    algorithm: "zopfli",
-    test: /\.(js|html)$/,
-    threshold: 10240,
-    minRatio: 0.8
-  })]
+    entry: './js/app.js',
+    mode: 'production',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            loaders: ['style-loader', 'css-loader']
+        }, {
+            test: /(fonts|images)/,
+            loaders: ['url-loader']
+        }]
+    },
+    plugins: [new UglifyJsPlugin({
+        test: /\.js($|\?)/i
+    }), new ZopfliPlugin({
+        asset: "[path].gz[query]",
+        algorithm: "zopfli",
+        test: /\.(js|html)$/,
+        threshold: 10240,
+        minRatio: 0.8
+    })]
 };
