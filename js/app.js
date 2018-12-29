8cec919ebd9b9a93144d82e98e33b7a04704454f
JonLuca DeCaro <jdecaro@usc.edu>
2018-12-29 10:15:48 +0100

    Better minify of jquery, added projects

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ require('../css/ie9.css');
 require('../css/font-awesome.min.css');
 
 window.$ = require('jquery');
-window.jQuery = require('jquery');
+window.jQuery = window.$;
 window.skel = require('./skel');
 require('./main.js');
 require('./util.js');
